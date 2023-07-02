a83771b7d6e5309ee5921a3d8366ea9789b91483
ParithiDB <samu.aidindia@gmail.com>
2023-07-02 20:39:49 +0530

    created component

--- src/App.js
+++ src/App.js
@@ -130,28 +130,14 @@ function App() {
         <div className='row'>
         {filterTodos().map((todo) => (
          
-         <div className='col-4'>
-          <div className="card w-50" key={todo.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
-            <p><b>Name : </b>{todo.taskName}</p>
-            <p><b>Description : </b>{todo.description}</p>
-            <div className='mb-5'>
-              <label for="status" className='me-1'><b>Status : </b></label>
-            <select
-              value={todo.status}
-              name='status'
-              onChange={(e) => updateStatus(todo.id, e.target.value)}
-            >
-              <option value="Completed" className='opt-complete'>Completed</option>
-              <option value="Not Completed" className='opt-not'>Not Completed</option>
-            </select>
-            </div>
-            <div class="d-grid gap-2 d-md-block ed-del-but">
-  <button className="btn but-edit me-3" onClick={() => editTodo(todo.id)} type="button">Edit</button>
-  <button className="btn butt" onClick={() => deleteTodo(todo.id)} type="button">Delete</button>
-</div>
-          </div>
-          </div>
-         
+         <div className='col-4' key={todo.id}>
+                <Todo
+                  todo={todo}
+                  updateStatus={updateStatus}
+                  editTodo={editTodo}
+                  deleteTodo={deleteTodo}
+                />
+              </div>
         ))}
       </div>
     </div>
--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Todo() {
-    const [todos, setTodos] = useState([]);
-
-    function handletodos() {
-        const input = document.getElementById("task").value;
-        const copy = [...todos];
-        copy.push(input);
-        setTodos(copy);
-    }
-
+export default function Todo({ todo, updateStatus, editTodo, deleteTodo }) {
     return (
-        <div>
-            <div className="inp">
-                <input id="task" placeholder="enter the task" />
-                <button onClick={handletodos}>Add</button>
+        <div className='col-4'>
+          <div className="card w-50" key={todo.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
+            <p><b>Name : </b>{todo.taskName}</p>
+            <p><b>Description : </b>{todo.description}</p>
+            <div className='mb-5'>
+              <label for="status" className='me-1'><b>Status : </b></label>
+            <select
+              value={todo.status}
+              name='status'
+              onChange={(e) => updateStatus(todo.id, e.target.value)}
+            >
+              <option value="Completed" className='opt-complete'>Completed</option>
+              <option value="Not Completed" className='opt-not'>Not Completed</option>
+            </select>
             </div>
-            <div className="disp">
-                {todos.map((d, i) => (
-                    <ul>
-                        <li key={`${i}`}>{d}</li>
-                    </ul>
-                ))}
+            <div class="d-grid gap-2 d-md-block ed-del-but">
+                <button className="btn but-edit me-3" onClick={() => editTodo(todo.id)} type="button">Edit</button>
+                <button className="btn butt" onClick={() => deleteTodo(todo.id)} type="button">Delete</button>
             </div>
-        </div>
+          </div>
+          </div>
+         
     )
 }
